6a0718af4bcbe3f89769422d16fedb28e67c3228
lunamayoral <lunabookblog@gmail.com>
2025-02-26 13:29:37 +0100

    ejs 2 3 b

---
+++ Test/Java/es/uah/mat/comp/mp/ejb/e3/AuthorTest.java
@@ -0,0 +1,33 @@
+package es.uah.mat.comp.mp.ejb.e3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorTest {
+    @Test
+    void getName() {
+        Author author = new Author("Mercedes Ron", "mercedes.ron@gmail.com");
+        assertEquals("Mercedes Ron", author.getName());
+    }
+
+    @Test
+    void getEmail() {
+        Author author = new Author("Mercedes Ron", "mercedes.ron@gmail.com");
+        assertEquals("mercedes.ron@gmail.com", author.getEmail());
+    }
+
+    @Test
+    void setEmail() {
+        Author author = new Author("Mercedes Ron", "mercedes.ron@gmail.com");
+        author.setEmail("mercedes.rn@gmail.com");
+        assertEquals("mercedes.rn@gmail.com", author.getEmail());
+    }
+
+    @Test
+    void testToString() {
+        Author author = new Author("Mercedes Ron", "mercedes.ron@gmail.com");
+        assertEquals("Author [name=Mercedes Ron, email=mercedes.ron@gmail.com]", author.toString());
+    }
+
+}
---
+++ src/es/uah/mat/comp/mp/ejb/e3/Book.java
@@ -0,0 +1,61 @@
+package es.uah.mat.comp.mp.ejb.e3;
+
+public class Book {
+    private String isbn;
+    private String name;
+    private Author author;
+    private double price;
+    private int qty;
+
+    public Book(String isbn, String name, Author author, double price) {
+        this.isbn = isbn;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = 0;
+    }
+
+    public Book(String isbn, String name, Author author, double price, int qty) {
+        this.isbn = isbn;
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String getAuthorName() {
+        return author.getName();
+    }
+
+    public String toString() {
+        return "Book [isbn=" + isbn + ", name=" + name + ",Author[" + author.toString() + "],price=" + price + ", qty=" + qty + "]";
+    }
+}
